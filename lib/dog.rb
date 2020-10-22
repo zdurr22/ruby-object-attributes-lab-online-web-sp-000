5d126b7b7d077cd40fca459671b87c95f64ac515
Zachary Durr <zdurr22@gmail.com>
2020-10-22 00:40:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name=(dogs_name)
+    @dogs_name = dogs_name
+  end
+
+  def name
+    @dogs_name
+  end
+
+  def breed=(dogs_breed)
+    @dogs_breed = dogs_breed
+  end
+
+  def breed
+    @dogs_breed
+  end
+
+  
+end
